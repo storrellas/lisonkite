d3d1a72a8483fdfc655646ccb38d2f59087eaa83
sergit <storrellas@gmail.com>
2015-01-23 10:27:41 +0100

    added files

---
+++ phpti_test/base.php
@@ -0,0 +1,8 @@
+<?php require_once 'ti.php' ?> 
+<html> 
+<body> 
+<div id='main'> <?php emptyblock('main') ?> </div> 
+<div id='footer'> <?php startblock('footer') ?> Copyright 2010 <?php endblock() ?> 
+</div> 
+</body> 
+</html>
---
+++ phpti_test/page.php
@@ -0,0 +1,3 @@
+<?php include 'base.php' ?> 
+<?php startblock('main') ?> The main content <?php endblock() ?> 
+<?php startblock('footer') ?> Custom footer!!! <?php endblock() ?>
