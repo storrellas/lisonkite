d37ffef652774cf145cbe43faf3726fe02998194
sergi <sergi@sergi-laptop>
2015-01-24 21:28:54 +0100

    - Added tela.php

---
+++ tela.php
@@ -0,0 +1,244 @@
+<?php include 'base.php' ?> 
+
+
+<?php startblock('content') ?> 
+<div class="art-layout-wrapper clearfix">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <div class="art-layout-cell art-content clearfix"><article class="art-post art-article">
+                                <div class="art-postmetadataheader">
+                                        <h2 class="art-postheader">Tela...</h2>
+                                    </div>
+                                                
+                <div class="art-postcontent art-postcontent-0 clearfix"><div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%">
+        <span style="font-size: 18px;"><br>
+        Existe variedad de procedimientos para reparar la rotura de nuestro kite, por este motivo evaluamos cada kite para poder ofrecerte la reparación más adecuada al estado de tu kite acorde con tu bolsillo.&nbsp;<br>
+        <br>
+        Las más comunes son...</span> <span style="font-size: 18px;"><br></span> <span style="font-size: 18px;"></span><br>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%">
+        <h1><span style="font-family: Tahoma; font-size: 18px; color: rgb(0, 0, 0);">- Reparación con cinta.</span><br></h1>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 33%">
+        <p> </p>
+        <br>
+        <p> </p>
+<div id="C49" style="position: relative; display: inline-block; z-index: 0; margin: 5px;  border-style: solid; border-color: #000000; border-width: 1px;  " class="art-collage">
+<div class="art-slider art-slidecontainerC49">
+    <div class="art-slider-inner">
+<div class="art-slide-item art-slideC490">
+
+</div>
+<div class="art-slide-item art-slideC491">
+
+</div>
+<div class="art-slide-item art-slideC492 active">
+
+</div>
+<div class="art-slide-item art-slideC493">
+
+</div>
+
+    </div>
+</div>
+<div class="art-slidenavigator art-slidenavigatorC49">
+<a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem active"></a><a href="#" class="art-slidenavigatoritem"></a>
+</div>
+
+
+
+    </div>
+
+        <p><span style="font-size: 18px;"><br></span></p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 67%">
+        <p>         </p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Las reparaciones con cinta son muy habituales debido a que esconden y refuerzan la zona de la rotura.&nbsp;</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Aplicando la cinta se enmascara la zona haciendo coincidir al mm el perfil original del kite. Tras esto se asegura toda la reparación de la vela utilizando la misma puntada original de nuestro kite.&nbsp;</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Para garantizar una reparación de por vida en todas nuestras intervenciones utilizamos hilo náutico con tratamientos especiales que lo hacen sumamente resistente a las inclemencias náuticas, como son los ultravioletas y la acritud de la sal.</span></p>
+        <p><span style="font-family: Tahoma; font-size: 18px;"></span></p>
+        <p><span style="font-family: Tahoma; font-size: 18px;"></span><br></p>
+        <p><span style="font-family: Tahoma; font-size: 18px;"><br></span></p>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%">
+        <h1><span style="font-size: 18px; color: #000000;">-Substitución de paneles.</span><br></h1>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 33%">
+        <p> </p>
+<div id="GaastraMax21" style="position: relative; display: inline-block; z-index: 0; margin: 5px;  border-style: solid; border-color: #000000; border-width: 1px;  " class="art-collage">
+<div class="art-slider art-slidecontainerGaastraMax21">
+    <div class="art-slider-inner">
+<div class="art-slide-item art-slideGaastraMax210">
+
+</div>
+<div class="art-slide-item art-slideGaastraMax211">
+
+</div>
+<div class="art-slide-item art-slideGaastraMax212 active">
+
+</div>
+<div class="art-slide-item art-slideGaastraMax213">
+
+</div>
+<div class="art-slide-item art-slideGaastraMax214">
+
+</div>
+<div class="art-slide-item art-slideGaastraMax215">
+
+</div>
+
+    </div>
+</div>
+<div class="art-slidenavigator art-slidenavigatorGaastraMax21">
+<a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem active"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a>
+</div>
+
+
+
+    </div>
+ <span style="font-size: 18px;"><br></span>
+        <p>         </p>
+        <p><span style="font-size: 18px;"></span></p>
+        <p><span style="font-size: 18px;"></span></p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 67%">
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Esta técnica de reparación es la que elimina literalmente la zona dañada de nuestro kite.&nbsp;</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Se trata de "amputar" el panel que ha sufrido el deterioro e implantar un nuevo panel que cumpla las mismas exigencias que el anterior.&nbsp;</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Por su complejidad requiere de un trabajo paciente y laborioso. Los elementos con los que se trabaja son conceptos como los estiramientos del nuevo panel, el tip natural de la vela, direcciones del tejido, refuerzos...&nbsp;</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Los paneles pueden ser de dimensiones reducidas o de mayor tamaño.</span></p>
+        <p><span style="font-size: 18px;"></span></p>
+        <p><span style="font-size: 18px;"><br></span></p>
+        <p> </p>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%">
+        <h1><span style="font-size: 18px; color: #000000;">-Zonas de presión.</span><br></h1>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 33%">
+        <p> </p>
+<div id="BAC4" style="position: relative; display: inline-block; z-index: 0; margin: 5px;  border-style: solid; border-color: #000000; border-width: 1px;  " class="art-collage">
+<div class="art-slider art-slidecontainerBAC4">
+    <div class="art-slider-inner">
+<div class="art-slide-item art-slideBAC40">
+
+</div>
+<div class="art-slide-item art-slideBAC41 active">
+
+</div>
+<div class="art-slide-item art-slideBAC42">
+
+</div>
+<div class="art-slide-item art-slideBAC43">
+
+</div>
+<div class="art-slide-item art-slideBAC44">
+
+</div>
+<div class="art-slide-item art-slideBAC45">
+
+</div>
+
+    </div>
+</div>
+<div class="art-slidenavigator art-slidenavigatorBAC4">
+<a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem active"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a>
+</div>
+
+
+
+    </div>
+
+        <p><br></p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 67%">
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Las zonas de presión están integradas por el borde de ataque y las costillas. Es una estructura textil que trabaja en cada sesión a una presión de expansión de entre 4 a 6 psi.</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">Es importante asimilar que si la cometa explosiona, es debido a:</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">-Costuras desgastadas o con el hilo roto o podrido por la humedad.</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">-Roces o cortes en el tejido "dacron" que debilita su resistencia.</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Tahoma, sans-serif;">-Desgaste prematuro, por pliegue repetido en la misma zona.&nbsp;</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Arial, sans-serif;">El modo de actuación puede ser substituir parte del panel, o bien reforzar interna y externamente el tejido primario.</span></p>
+        <p style="margin-top: 9pt; margin-right: 3.75pt; margin-bottom: 9pt; margin-left: 3.75pt; line-height: 14.25pt;"><span style="font-size: 13.5pt; font-family: Arial, sans-serif;">De ciencia cierta, un pequeño corte en el dacron puede ocasionarnos una gran explosión del blader y desgarro del dacron. Por este motivo es conveniente tomar precauciones antes de aplicarle presión.</span></p>
+        <p><span style="font-size: 18px;"></span></p>
+        <p><span style="font-family: Arial; font-size: 18px;"></span></p>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%">
+        <p> </p>
+        <p> </p>
+        <h1><span style="color: rgb(0, 0, 0); font-weight: bold; font-size: 18px;">-Zonas de tracción.</span></h1>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 33%">
+<div id="Air1" style="position: relative; display: inline-block; z-index: 0; margin: 5px;  border-style: solid; border-color: #000000; border-width: 1px;  " class="art-collage">
+<div class="art-slider art-slidecontainerAir1">
+    <div class="art-slider-inner">
+<div class="art-slide-item art-slideAir10">
+
+</div>
+<div class="art-slide-item art-slideAir11 active">
+
+</div>
+<div class="art-slide-item art-slideAir12">
+
+</div>
+<div class="art-slide-item art-slideAir13">
+
+</div>
+
+    </div>
+</div>
+<div class="art-slidenavigator art-slidenavigatorAir1">
+<a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem active"></a><a href="#" class="art-slidenavigatoritem"></a><a href="#" class="art-slidenavigatoritem"></a>
+</div>
+
+
+
+    </div>
+ <span style="font-family: Tahoma; font-size: 18px;"><br></span>
+    </div><div class="art-layout-cell layout-item-0" style="width: 67%">
+        <p> </p>
+        <p class="MsoNormal"><span style="font-size: 13.5pt; line-height: 107%; font-family: Tahoma, sans-serif;">Son los puntos de anclaje a la vela, donde divide la tracción y se reparte por toda la superficie del tejido.</span><span style="font-size: 13.5pt; line-height: 107%; font-family: Tahoma, sans-serif;"><br>
+        <br>
+        Normalmente suceden como efecto secundario tras la rotura de una línea en condiciones de viento fuerte.<br>
+        <br>
+        Es frecuente la rotura del blader.</span></p>
+        <p style="text-align: left;"><span style="font-family: Tahoma; font-size: 18px; text-align: left;"></span><span style="white-space: nowrap;"><br></span></p>
+    </div>
+    </div>
+</div>
+</div>
+                
+</article></div>
+                    </div>
+                </div>
+            </div>
+
+<?php endblock() ?>			 
