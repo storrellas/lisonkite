220dd1e62cd954e246cc18dcd8c84cd85e4d2462
sergi <sergi@sergi-laptop>
2015-01-24 21:20:34 +0100

    - Added reparaciones.php

---
+++ reparaciones.php
@@ -0,0 +1,46 @@
+<?php include 'base.php' ?> 
+
+
+<?php startblock('content') ?> 
+<div class="art-layout-wrapper clearfix">
+	<div class="art-content-layout">
+		<div class="art-content-layout-row">
+			<div class="art-layout-cell art-content clearfix"><article class="art-post art-article">
+				<div class="art-postmetadataheader">
+					<h2 class="art-postheader">Qué tipo de reparación necesitas......</h2>
+				</div>
+                                                
+                <div class="art-postcontent art-postcontent-0 clearfix">
+                	<div class="art-content-layout">
+    					<div class="art-content-layout-row">
+    						<div class="art-layout-cell" style="width: 100%" >
+        						<br>
+    						</div>
+    					</div>
+					</div>
+					<div class="art-content-layout">
+    					<div class="art-content-layout-row">
+    						<div class="art-layout-cell" style="width: 50%" >
+        						<p><a href="reparaciones/tela.html"><img width="400" height="200" alt="" src="images/Tejido-2.jpg" class=""></a><br></p>
+    						</div>
+    						<div class="art-layout-cell" style="width: 50%" >
+        						<p><a href="reparaciones/flotadores-2.html"><img width="400" height="200" alt="" src="images/Flotador.jpg" class=""></a><br></p>
+    						</div>
+    					</div>
+					</div>
+					<div class="art-content-layout">
+    					<div class="art-content-layout-row">
+    						<div class="art-layout-cell" style="width: 50%" >
+        						<p><a href="reparaciones/barras-2.html"><img width="400" height="200" alt="" src="images/Barras-2.jpg" class=""></a><br></p>
+    						</div>
+    						<div class="art-layout-cell" style="width: 50%" >
+        						<p><a href="reparaciones/presupuesto.html"><img width="400" height="200" alt="" src="images/Presupuesto-2.jpg" class=""></a><br></p>
+    						</div>
+    					</div>
+					</div>
+				</div>               
+			</div>
+		</div>
+	</div>
+</div>                
+<?php endblock() ?>	
