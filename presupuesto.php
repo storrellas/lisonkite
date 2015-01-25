6ef9004932c678cf2618f1641ef8952f1dbf71ff
sergi <sergi@sergi-laptop>
2015-01-25 19:44:06 +0100

    - Presupuesto and formulario with personalized title

--- presupuesto.php
+++ presupuesto.php
@@ -1,5 +1,8 @@
 <?php include 'base.php' ?>
 
+<?php startblock('title') ?>
+Presupuesto reparación kite
+<?php endblock() ?>
 
 <?php startblock('content') ?>
 <div class="art-layout-wrapper clearfix">
