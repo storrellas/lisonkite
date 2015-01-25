ddd03f036973beb5d9fd13ae073ecb70f9a4943e
sergi <sergi@sergi-laptop.(none)>
2015-01-25 19:24:42 +0100

    - Redirects to php version

--- base.php
+++ base.php
@@ -85,75 +85,25 @@
 		<!-- Navigation panel -->
 		<nav class="art-nav clearfix">
 		    <ul class="art-hmenu">
-		    	<li><a href="inicio.html" class="active">INICIO</a></li>
-		    	<li><a href="reparaciones.html">REPARACIONES</a>
+		    	<li><a href="index.php" class="active">INICIO</a></li>
+		    	<li><a href="reparaciones.php">REPARACIONES</a>
 				<ul>
-			    	<li><a href="reparaciones/tela.php">TELA</a></li>
-			    	<li><a href="reparaciones/flotadores-2.html">FLOTADORES</a></li>
-			    	<li><a href="reparaciones/barras-2.html">BARRAS</a></li>
-			    	<li><a href="reparaciones/presupuesto.html">PRESUPUESTO</a></li>
+			    	<li><a href="tela.php">TELA</a></li>
+			    	<li><a href="flotadores.php">FLOTADORES</a></li>
+			    	<li><a href="barras.php">BARRAS</a></li>
+			    	<li><a href="presupuesto.php">PRESUPUESTO</a></li>
 		    	</ul>
 		    	</li>
-		    	<li><a href="recogidas.html">RECOGIDAS</a></li>
-		    	<li><a href="kite-style.html">KITE STYLE</a></li>
-		    	<li><a href="quienes-somos.html">QUIENES SOMOS</a></li>
+		    	<li><a href="recogidas.php">RECOGIDAS</a></li>
+		    	<li><a href="kite-style.php">KITE STYLE</a></li>
+		    	<li><a href="quienes-somos.php">QUIENES SOMOS</a></li>
 		    	<li><a href="formulario.php">FORMULARIO</a></li>
-		    	<li><a href="contacto.html">CONTACTO</a></li>		    
+		    	<li><a href="contacto.php">CONTACTO</a></li>		    
 		    </ul> 
 		</nav>
 
 		<div class="art-layout-wrapper clearfix">
 			<?php startblock('content') ?> Place here your content <?php endblock() ?> 
-			<!-- 
-        	<div class="art-content-layout">
-            	<div class="art-content-layout-row">
-					<div class="art-layout-cell art-content clearfix">
-						<article class="art-post art-article">
-							<div class="art-postcontent art-postcontent-0 clearfix">
-								<div class="art-content-layout">
-    								<div class="art-content-layout-row">
-    									<div class="art-layout-cell" style="width: 100%" >
-        									<h2 style="text-align: center;">
-        										<span style="line-height: 19.5px; text-align: center; color: rgb(46, 30, 45); background-color: rgb(244, 246, 244);">En LisonKite te ofrecemos el servicio de reparación integral de tu material de kite.</span>
-        											<br style="line-height: 19.5px; text-align: center; color: rgb(46, 30, 45); background-color: rgb(244, 246, 244);">
-        											<br style="line-height: 19.5px; text-align: center; color: rgb(46, 30, 45); background-color: rgb(244, 246, 244);">
-        											<span style="line-height: 19.5px; text-align: center; color: rgb(46, 30, 45); background-color: rgb(244, 246, 244);">Si tienes tu equipo deteriorado consulta el tipo de trámite que te podemos ofrecer para que lo tengas en óptimas condiciones lo antes possible. Mensajería, recogida / entrega desde 8€ por trayecto, en 24h.</span>
-        											<br>
-        									</h2>
-        									<p style="text-align: center;">
-        										<span style="line-height: 19.5px; text-align: center; color: rgb(46, 30, 45); background-color: rgb(244, 246, 244);"><br></span>
-											</p>
-    									</div>
-    								</div>
-								</div>
-								<div class="art-content-layout layout-item-0">
-								    <div class="art-content-layout-row">
-								    <div class="art-layout-cell layout-item-1" style="width: 33%" >
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;"><span style="font-family: 'Arial Black'; font-size: 26px; font-weight: bold; color: rgb(0, 0, 0);">RECOGIDAS</span></p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">Solicita una recogida para mañana y te enviaremos un mensajero a la dirección que nos indiques.&nbsp;</p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">En 24h tendremos tu material y te llamaremos para concretar la reparación mas adecuada y su presupuesto.</p>
-								        <p><br></p>
-								    </div><div class="art-layout-cell layout-item-1" style="width: 34%" >
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;"><span style="font-family: 'Arial Black'; font-size: 26px; font-weight: bold;">RECAMBIOS</span><br></p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">Solicita el recambio que necesitas y repáratelo tu mismo, válido para:<br></p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">Cabos, líneas, bridas, pig-tails,&nbsp;válvulas auto adhesivas, pig-tails, alargos, costillas, bordes de ataque, telas adhesivas...</p>
-								        <p style="text-align: center;"><br></p>
-								    </div><div class="art-layout-cell layout-item-1" style="width: 33%" >
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;"><span style="font-family: 'Arial Black'; font-size: 26px; font-weight: bold;">CONSULTAS</span></p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">Dudas o consultas contacta con nosotros:</p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">lisonkite@lisonkite.com</p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">Llámanos o escríbenos un &nbsp;WhatsApp</p>
-								        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">654.84.38.71</p>
-								        <p><br></p>
-								    </div>
-								    </div>
-								</div>
-							</div>                
-						</article>
-					</div>
-				</div>
-			</div>
-			 -->
 		</div>
 
 		<!-- Footer -->
