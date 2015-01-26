89a5646c79c5f7f036abb530acdec52a81304654
sergi <sergi@sergi-laptop.(none)>
2015-01-26 20:09:07 +0100

    - Added a little bit of JS

--- formulario.php
+++ formulario.php
@@ -5,6 +5,46 @@ Presupuesto reparación kite
 <?php endblock() ?>
 
 <?php startblock('content') ?>
+
+<script> 
+
+// Holds the current page
+current_page = 1;
+
+	$(document).ready(function(){
+	});
+
+
+// Name     : show_page
+// Function : shows the page indicated by n_page	
+function show_page(n_page){
+	
+}
+	
+// Name     : page_switch
+// Function : switches from one page to the next one
+function page_switch(command){
+	if( command == "next" ){
+		current_page++;
+	}else{
+		current_page--;
+	}	
+	console.log("Current page is now " + current_page)
+
+	// Adapt view to current page
+	switch(current_page){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+	}
+
+	
+}
+
+</script>
   
 <div class="art-layout-wrapper clearfix">
 
@@ -49,8 +89,14 @@ Presupuesto reparación kite
 	
 	<!-- Form navigation -->
 	<div style="background: red; text-align:center;">
+
+	 	<img src="images/arrow_left_disabled.png"  style="cursor:pointer; margin-right: 20px" height="80" width="80" onclick="page_switch('previous')"> 
+	 	<img src="images/arrow_right_enabled.png" style="cursor:pointer; margin-left: 20px" height="80" width="80" onclick="page_switch('next')">
+	 	
+	 	<!-- 
 	 	<img src="images/arrow_left_disabled.png"  style="cursor:pointer; margin-right: 20px" height="80" width="80" onclick="location.href='ok.php'"> 
 	 	<img src="images/arrow_right_enabled.png" style="cursor:pointer; margin-left: 20px" height="80" width="80" onclick="location.href='ok.php'">
+	 	 -->
 	 	<!-- 
 		<a href="ok.php" >back button</a>
 		<a href="ok.php" >next button</a>
