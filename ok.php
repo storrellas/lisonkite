69c02a9de626f9830e0f3e3e0941d2aee260436c
sergi <sergi@sergi-Aspire-5742>
2015-02-04 20:36:25 +0100

    - Added image to ok.php

--- ok.php
+++ ok.php
@@ -9,20 +9,19 @@ LisonKite
 	<div class="art-content-layout">
     	<div class="art-layout-cell layout-item-1" style="width: 34%">
 	<br>
-	<br>
-	<br>
-	<br>
-	
-	
-	
-        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;"><span style="font-family: 'Arial Black'; font-size: 26px; font-weight: bold;">SOLICITUD ENVIADA CON EXITO ! !</span><br></p>
-        <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">Concertaremos la recogida y te avisaremos por e-mail.  Gracias.<br></p>
-        <p style="text-align: center;"><br></p>
         
-        <div id=id1 style="width: 90%; height: 200px; background-color:red; margin-left:5%;" >
-        	<span style="line-height: 300px;background-color:blue; width:100%; height:100%">sdfalj</span><!-- <span><img width="200px" height="200px" alt="" src="images/ok/ok.jpg"></span>-->
-        	
-        	
+        <div id=id1 style="width: 90%; height: 200px; margin-left:5%; ">
+        	<div style="margin-left: 100px; width:500px; margin-top: 50px; float:left;text-align:center">
+        		<div style="font-family: 'Arial Black'; font-size: 26px; font-weight: bold;">
+        		SOLICITUD ENVIADA CON EXITO ! !
+        		</div> 
+        		<div style="font-size: 14px;">
+        		Concertaremos la recogida y te avisaremos por e-mail.  Gracias.
+        		</div>        	        	
+        	</div>
+        	<div style="float:left;line-height: 200px; background-color:blue; height:200px">
+        		<img width="200px" height="200px" alt="" src="images/ok/ok.jpg">
+        	</div>
         </div>
         
 		</div>
