dfab4f19be04679eef829ef4f39c08b3314d4430
sergit <storrellas@gmail.com>
2015-02-04 10:06:17 +0100

    - Added formulario items

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -1,28 +1,55 @@
 
 <?php
 
-//se mandan a llamar las variables con metodo POST 
-$nombre = $_POST["nombre"]; 
-$apellidos = $_POST["apellidos"]; 
-$telefono = $_POST["telefono"]; 
-$direccion = $_POST["direccion"]; 
-$codigo = $_POST["codigo"];
-$poblacion = $_POST["poblacion"]; 
-$email = $_POST["email"]; 
-$descripcion = $_POST["descripcion"]; 
-$horario = $_POST["horario"]; 
+// Page0 Items
+$question1 = $_POST["question1"];
+$question2 = $_POST["question2"];
+$question3 = $_POST["question3"];
+$question4 = $_POST["question4"];
+
+$question5 = $_POST["question5"];
+$question6 = $_POST["question6"];
+$question7 = $_POST["question7"];
+$question8 = $_POST["question8"];
+
+// Page1 Items 
+$nombre       = $_POST["nombre"]; 
+$apellidos    = $_POST["apellidos"]; 
+$telefono     = $_POST["telefono"]; 
+$direccion    = $_POST["direccion"];
+ 
+$codigo       = $_POST["codigo"];
+$poblacion    = $_POST["poblacion"]; 
+$email        = $_POST["email"]; 
+$doubts       = $_POST["doubts"]; 
+$contact_type = $_POST["contact_type"]; 
+
 
 
 //se incluyen las variables del form en el mensaje 
-$mensaje ="NOMBRE:". $nombre ."\n"; 
-$mensaje .="APELLIDOS: ".$apellidos." \n"; 
-$mensaje .="TELEFONO:".$telefono." \n"; 
-$mensaje .="DIRECCION:".$direccion." \n"; 
-$mensaje .="CODIGO POSTAL:".$codigo." \n"; 
-$mensaje .="POBLACION:".$poblacion." \n"; 
-$mensaje .="EMAIL:".$email." \n"; 
-$mensaje .="DESCRIPCION:".$descripcion." \n"; 
-$mensaje .="HORARIO RECOGIDA:".$horario." \n"; 
+$mensaje = "NOMBRE:". $nombre ."\n"; 
+$mensaje .= "APELLIDOS: ".$apellidos." \n"; 
+$mensaje .= "TELEFONO:".$telefono." \n"; 
+$mensaje .= "DIRECCION:".$direccion." \n"; 
+$mensaje .= "CODIGO POSTAL:".$codigo." \n"; 
+$mensaje .= "POBLACION:".$poblacion." \n"; 
+$mensaje .= "EMAIL:".$email." \n"; 
+$mensaje .= "DESCRIPCION:".$descripcion." \n"; 
+$mensaje .= "HORARIO RECOGIDA:".$horario." \n";
+$mensaje .= "\n";
+
+$mensaje .= "QUESTION1:".$question1."\n";
+$mensaje .= "QUESTION2:".$question2."\n";
+$mensaje .= "QUESTION3:".$question3."\n";
+$mensaje .= "QUESTION4:".$question4."\n";
+
+$mensaje .= "QUESTION5:".$question5."\n";
+$mensaje .= "QUESTION6:".$question6."\n";
+$mensaje .= "QUESTION7:".$question7."\n";
+$mensaje .= "QUESTION8:".$question8."\n";
+$mensaje .= "\n";
+
+
 
 $cabeceras = "From:	".$email."\n";
 
@@ -38,28 +65,37 @@ $destino = "sergi@localhost";
 $origen = $email;
 
 
+// Page for log
 echo ("<center><b><font size='5' color='#000000' face='Arial'>");
 
+echo ("<p>Question1:".$question1."</p>");
+echo ("<p>Question2:".$question2."</p>");
+echo ("<p>Question3:".$question3."</p>");
+echo ("<p>Question4:".$question4."</p>");
+echo ("<p>Question5:".$question5."</p>");
+echo ("<p>Question6:".$question6."</p>");
+echo ("<p>Question7:".$question7."</p>");
+echo ("<p>Question8:".$question8."</p>");
 
-echo ("<p>Question1:".$_POST["page0_question1"]."</p>");
-echo ("<p>Question2:".$_POST["page0_question2"]."</p>");
-echo ("<p>Question3:".$_POST["page0_question3"]."</p>");
-echo ("<p>Question4:".$_POST["page0_question4"]."</p>");
-echo ("<p>Question5:".$_POST["page0_question5"]."</p>");
-echo ("<p>Question6:".$_POST["page0_question6"]."</p>");
-echo ("<p>Question7:".$_POST["page0_question7"]."</p>");
-echo ("<p>Question8:".$_POST["page0_question8"]."</p>");
+echo ("<p>Nombre:".$nombre."</p>");
+echo ("<p>Apellidos:".$apellidos."</p>");
+echo ("<p>Telefono:".$telefono."</p>");
+echo ("<p>direccion:".$direccion."</p>");
+echo ("<p>codigo:".$codigo."</p>");
+echo ("<p>poblacion:".$poblacion."</p>");
+echo ("<p>email:".$email."</p>");
+echo ("<p>doubts:".$doubts."</p>");
+echo ("<p>contact_type:".$contact_type."</p>");
+
+echo ("</b></font></center>")
 
 
-
-/*
+// Send the email
 if (mail($destino,$origen,$cuerpo,$cabeceras)) 
 { 
-//echo header( 'Location: http://lisonkite.com/ok.html' ) ;
-echo header( 'Location: ok.php' ) ;
+	echo header( 'Location: ok.php' ) ;
 } else { 
-echo ("ERROR!!. PORFAVOR, SOLICITA TU RECOGIDA POR WhatsApp o Mail."); 
+	echo ("ERROR!!. PORFAVOR, SOLICITA TU RECOGIDA POR WhatsApp o Mail."); 
 } 
-/**/
 
 ?> 
--- formulario.php
+++ formulario.php
@@ -91,8 +91,8 @@ Presupuesto reparación kite
 					Moldea el flotador hasta que esten las selladas bien colocadas en el borde.<p>
 				
 					<div>
-					 <input type="radio" name="page0_question1" value="question1_A" checked="checked">A - Valvula a la Izquierda<br>
-					 <input type="radio" name="page0_question1" value="question1_B">B - Valvula a la Derecha<br>
+					 <input type="radio" name="question1" value="question1_A" checked="checked">A - Valvula a la Izquierda<br>
+					 <input type="radio" name="question1" value="question1_B">B - Valvula a la Derecha<br>
 					</div>
 					<div>
 						<img src="images/kitesurfing-273315_640.jpg" style="width: 250px; margin: 0px auto; display: block;">
@@ -103,13 +103,13 @@ Presupuesto reparación kite
 					<br>
 					<p><b>Longitud maxima de la costilla de punta a punta. *</b></p>
 					<p>Toma la medida total de la costilla. De exterior a exterior.</p>
-					<input type="text" name="page0_question2"/>
+					<input type="text" name="question2"/>
 				</div>
 				
 	
 				<div id="question3" class="question">
 					<p><b>Ancho de la costilla en el lado más estrecho *</b></p>
-					<input type="text" name="page0_question3"/>
+					<input type="text" name="question3"/>
 				</div>
 				
 				
@@ -118,31 +118,31 @@ Presupuesto reparación kite
 					<p>Cada diseño de cometa fabrica su punta de diferente manera, la forma de que encaje 
 					a la perfeccion la costilla generica es conseguir ocupar todo el espacio del interior. 
 					Unos ejemplos en la foto!</p>
-					<input type="text" name="page0_question4"/>
+					<input type="text" name="question4"/>
 				</div>				
 				
 				<div id="question5" class="question">
 					<p><b>Centrado de la costilla en horizontal *</b></p>
 					<p>Desde el centro de la valvula hasta la punta mas ancha de la costilla.</p>
-					<input type="text" name="page0_question5"/>
+					<input type="text" name="question5"/>
 				</div>				
 				
 				<div id="question6" class="question">
 					<p><b>Centrado de la costilla en vertical *</b></p>
 					<p>Desde el centro de la valvula hasta la sellada en la base de la costilla.</p>
-					<input type="text" name="page0_question6"/>
+					<input type="text" name="question6"/>
 				</div>				
 				
 				<div id="question7" class="question">
 					<p><b>Tipo de válvula para tu costilla *</b></p>
 					<p>Desde el centro de la valvula hasta la sellada en la base de la costilla.</p>
 					<div>
-					 <input type="radio" name="page0_question7" value="question6_A" checked="checked">A - Standard sin stoper<br>
-					 <input type="radio" name="page0_question7" value="question6_B">B - Standard con stoper de membrana ( retiene el aire si sacas el inflador )<br>
-					 <input type="radio" name="page0_question7" value="question6_C">C - Standard con stoper de bola ( retiene el aire si sacas el inflador )<br>
-					 <input type="radio" name="page0_question7" value="question6_D">D - One pump negro ( Ozone, Slingshot.... )<br>
-					 <input type="radio" name="page0_question7" value="question6_E">E - One pump azul ( Best, North.... )<br>
-					 <input type="radio" name="page0_question7" value="question6_F">F - One Pump Redonda ( Best )<br>			 
+					 <input type="radio" name="question7" value="question6_A" checked="checked">A - Standard sin stoper<br>
+					 <input type="radio" name="question7" value="question6_B">B - Standard con stoper de membrana ( retiene el aire si sacas el inflador )<br>
+					 <input type="radio" name="question7" value="question6_C">C - Standard con stoper de bola ( retiene el aire si sacas el inflador )<br>
+					 <input type="radio" name="question7" value="question6_D">D - One pump negro ( Ozone, Slingshot.... )<br>
+					 <input type="radio" name="question7" value="question6_E">E - One pump azul ( Best, North.... )<br>
+					 <input type="radio" name="question7" value="question6_F">F - One Pump Redonda ( Best )<br>			 
 					</div>						
 					<div style="margin-top:10px">
 						<img alt="" src="images/valvula-autoadhesiva.jpg" style="width: 500px; margin: 0px auto; display: block;">
@@ -152,10 +152,10 @@ Presupuesto reparación kite
 				<div id="question8" class="question">
 					<p><b>Orientacion del tapon en la valvula escogida. *</b></p>
 					<div>
-						<input type="radio" name="page0_question8" value="question7_A" checked="checked">A<br>
-				 		<input type="radio" name="page0_question8" value="question7_B">B<br>
-						<input type="radio" name="page0_question8" value="question7_C">C<br>
-						<input type="radio" name="page0_question8" value="question7_D">D<br>				
+						<input type="radio" name="question8" value="question7_A" checked="checked">A<br>
+				 		<input type="radio" name="question8" value="question7_B">B<br>
+						<input type="radio" name="question8" value="question7_C">C<br>
+						<input type="radio" name="question8" value="question7_D">D<br>				
 					</div>
 					<div>
 						<img alt="" src="images/soap.jpg" style="width: 300px; margin: 0px auto; display: block;">
@@ -165,67 +165,73 @@ Presupuesto reparación kite
 			</div>
 		</div>
 
-		<div align="center">
-			<input name="Reset" value="LIMPIAR FORMULARIO" type="reset"> 
-			<input name="Submit" value="ENVIAR" type="submit">			
+		<div id=page_1 style="padding: 20px">					
+			<div style="border: 3px solid black; border-radius: 20px; padding: 20px;">
+			 	
+				<div class="question">
+			 		<div class="title">Nombre:</div> 
+			 		<input value="" name="nombre" size="25" maxlength="90" type="text">		 
+			 	</div>
+				
+				<div class="question">
+					<div class="title">Apellidos:</div> 
+					<input value="" name="apellidos" size="35" maxlength="80" type="text">
+				</div>
+				
+				<div class="question">
+					<div class="title">Telefono:</div>			
+					<input value="" name="telefono" size="9" maxlength="9" type="text">
+				</div>
+				
+				<div class="question">
+					<div class="title">Direccion:</div> 
+					<input value="" name="direccion" size="25" maxlength="100" type="text"><br>
+				</div>
+				
+				<div class="question">
+					<div class="title">Codigo Postal:</div> 
+					<input value="" name="codigo" size="5" maxlength="5" type="text">			
+				</div>
+				
+				<div class="question">
+					<div class="title">Población:</div>
+				 	<input value="" name="poblacion" size="15" maxlength="50" type="text">			
+				</div>
+				
+				<div class="question">
+					<div class="title">Correo electrónico:</div> 
+					<input value="@" name="email" size="40" maxlength="120" type="text">				
+				</div>
+				
+				<div class="question">
+					<div class="title">¿Alguna duda?</div>			
+					<textarea id="doubts" name="doubts" cols="40" rows="5"></textarea>
+				</div>
+				
+				<div class="question">
+					<div class="title">¿Cómo prefieres que nos pongamos en contacto contigo?</div>
+					<div>
+						<input type="radio" name="contact_type" value="whatsapp" checked="checked">Wassssssuppp<br>
+				 		<input type="radio" name="contact_type" value="telefono">Teléfono<br>
+						<input type="radio" name="contact_type" value="mail">mail<br>
+					</div>
+				</div>
+			
+				<!-- Submit form -->
+				<div align="center">
+					<input name="Reset" value="LIMPIAR FORMULARIO" type="reset"> 
+					<input name="Submit" value="ENVIAR" type="submit">			
+				</div>
+			
+			</div>
 		</div>
+
+
+
 	
 	</form>
 
-	<div id=page_1 style="padding: 20px">					
-		<div style="border: 3px solid black; border-radius: 20px; padding: 20px;">
-		 	
-			<div id="question1" class="question">
-		 		<div class="title">Nombre:</div> 
-		 		<input value="" name="nombre" size="25" maxlength="90" type="text">		 
-		 	</div>
-			
-			<div id="question2" class="question">
-				<div class="title">Apellidos:</div> 
-				<input value="" name="apellidos" size="35" maxlength="80" type="text">
-			</div>
-			
-			<div id="question3" class="question">
-				<div class="title">Telefono:</div>			
-				<input value="" name="telefono" size="9" maxlength="9" type="text">
-			</div>
-			
-			<div id="question4" class="question">
-				<div class="title">Direccion:</div> 
-				<input value="" name="direccion" size="25" maxlength="100" type="text"><br>
-			</div>
-			
-			<div id="question5" class="question">
-				<div class="title">Codigo Postal:</div> 
-				<input value="" name="codigo" size="5" maxlength="5" type="text">			
-			</div>
-			
-			<div id="question6" class="question">
-				<div class="title">Población:</div>
-			 	<input value="" name="poblacion" size="15" maxlength="50" type="text">			
-			</div>
-			
-			<div id="question7" class="question">
-				<div class="title">Correo electrónico:</div> 
-				<input value="@" name="email" size="40" maxlength="120" type="text">				
-			</div>
-			
-			<div id="question9" class="question">
-				<div class="title">¿Alguna duda?</div>			
-				<textarea name="descripcion" id="descripcion" cols="40" rows="5"></textarea>
-			</div>
-			
-			<div id="question9" class="question">
-				<div class="title">¿Cómo prefieres que nos pongamos en contacto contigo?</div>
-				<div>
-					<input type="radio" name="question7" value="question7_A">Wassssssuppp<br>
-			 		<input type="radio" name="question7" value="question7_B">Teléfono<br>
-					<input type="radio" name="question7" value="question7_C">mail<br>
-				</div>
-			</div>
-		
-		</div>
-	</div>
+
 <!-- 
 	<div id=page_2 style="padding: 20px">			
 		<div style="border: 3px solid black; border-radius: 20px; text-align: center; height: 300px; line-height: 300px;font-size:60px;">
