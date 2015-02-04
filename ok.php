9df5206e61ca3058a8842b371ef3a7b93f9526af
sergit <storrellas@gmail.com>
2015-02-04 19:53:29 +0100

    - Added ok.jpg and modified page

--- ok.php
+++ ok.php
@@ -6,17 +6,26 @@ LisonKite
 
 <?php startblock('content') ?> 
 <div class="art-layout-wrapper clearfix">
-                <div class="art-content-layout">
-                    <div class="art-content-layout-row">                                          
-    </div><div class="art-layout-cell layout-item-1" style="width: 34%">
+	<div class="art-content-layout">
+    	<div class="art-layout-cell layout-item-1" style="width: 34%">
 	<br>
 	<br>
 	<br>
 	<br>
+	
+	
+	
         <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;"><span style="font-family: 'Arial Black'; font-size: 26px; font-weight: bold;">SOLICITUD ENVIADA CON EXITO ! !</span><br></p>
         <p style="color: rgb(25, 16, 24); line-height: 19.5px; text-align: center;">Concertaremos la recogida y te avisaremos por e-mail.  Gracias.<br></p>
         <p style="text-align: center;"><br></p>
-    </div>
+        
+        <div id=id1 style="width: 90%; height: 200px; background-color:red; margin-left:5%;" >
+        	<span style="line-height: 300px;background-color:blue; width:100%; height:100%">sdfalj</span><!-- <span><img width="200px" height="200px" alt="" src="images/ok/ok.jpg"></span>-->
+        	
+        	
+        </div>
+        
+		</div>
     </div>
 </div>
 <?php endblock() ?>			 
